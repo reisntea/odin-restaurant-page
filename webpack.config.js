9ba034a6d35905d4143090935301a86612b33e6c
Brian C <283823263+reisntea@users.noreply.github.com>
2026-07-18 21:53:55 -0400

    Create homepage and add styling
    Had to edit the webpack to add the custom font for the heading and for
    the css file. The homepage is fully finished in html with styling.

--- webpack.config.js
+++ webpack.config.js
@@ -24,6 +24,18 @@ export default {
             test: /\.css$/i,
             use: ["style-loader", "css-loader"],
             },
+            {
+            test: /\.html$/i,
+            use: ["html-loader"],
+            },
+            {
+            test: /\.(png|svg|jpg|jpeg|gif)$/i,
+            type: "asset/resource",
+            },
+            {
+            test: /\.(woff|woff2|eot|ttf|otf)$/i,
+            type: 'asset/resource',
+            },
         ],
     },
 };
